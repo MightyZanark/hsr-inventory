e514cfe39c95491a32c7c59c213ee6fb058004d6
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2023-09-11 14:18:09 +0700

    add: Item model test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from main.models import Item
 
-# Create your tests here.
+class MainTest(TestCase):
+    def test_item_have_all_attribute(self):
+        item1 = Item.objects.create(
+                    name="Traveler's Guide",
+                    amount=100,
+                    description="Lorem ipsum",
+                    category="CHAR_EXP"
+                )
+        
+        self.assertEqual(item1.name, "Traveler's Guide")
+        self.assertEqual(item1.amount, 100)
+        self.assertEqual(item1.description, "Lorem ipsum")
+        self.assertEqual(item1.category, "CHAR_EXP")
