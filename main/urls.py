fb8087cf568c62107e7d072c2845c5ed8e1b6072
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2023-09-18 13:58:12 +0700

    add form

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, add_item
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main')
+    path('', show_main, name='show_main'),
+    path('add-item', add_item, name='add_item')
 ]
--- main/views.py
+++ main/views.py
@@ -1,24 +1,28 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.core import serializers
 from django.shortcuts import render
 
+from main.forms import ItemForm
+from main.models import Item
+
 def show_main(request: HttpRequest) -> HttpResponse:
+    items = Item.objects.all()
+    
     context = {
         'name': 'Juan Maxwell Tanaya',
         'class': 'C',
-        'inventory': [
-            {
-                'name': 'Traveler\'s Guide',
-                'amount': 163,
-            },
-            {
-                'name': 'Refined Aether',
-                'amount': 123,
-            },
-            {
-                'name': 'Lost Crytsal',
-                'amount': 321,
-            }
-        ]
+        'inventory': items,
     }
 
     return render(request, "main.html", context)
+
+def add_item(request: HttpRequest) -> HttpResponseRedirect | HttpResponse:
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "add_item.html", context)
